1d80c44b4e589a99ba986f8aef43b6b2bda86671
vitor-nery11 <vitornerysantos24@gmail.com>
2026-08-21 14:43:55 -0300

    feat: adicionando config.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask 
-
+from app.config import Config 
 from app.routes.tasks import tasks_bp 
 
 def create_app():
     app = Flask(__name__)
 
+    app.config.from_object(Config)
+    
     app.register_blueprint(tasks_bp)
 
     return app 
